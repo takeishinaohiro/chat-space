9f92df34e2679d6692cde2663ce10af2bd276fc3
takeishinaohiro <rurushuviburitaniaga214@gmail.com>
2019-12-02 15:57:09 +0900

    かんせい

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,28 @@
 app_path = File.expand_path('../../', __FILE__)
 
+worker_processes 1
+
+working_directory app_path
+pid "#{app_path}/tmp/pids/unicorn.pid"
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+ ↓↓↓↓↓↓↓ 以下のように変更 ↓↓↓↓↓↓
+
+# ../が一つ増えている
+app_path = File.expand_path('../../../', __FILE__)
+
+worker_processes 1
+# currentを指定
+working_directory "#{app_path}/current"
+
+# それぞれ、sharedの中を参照するよう変更
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 
@@ -10,7 +33,7 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
